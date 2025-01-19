fc58dfccea1bd6b1d9e19882f3703b8989f5ea0d
m.arvinfar <m.arvinfar@VDI-W10-BSS01>
2025-01-19 10:46:27 +0330

    first commit

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -14,91 +14,107 @@ using Newtonsoft.Json;
 using System.Web.Mvc;
 using System.Web.Configuration;
 using RouteAttribute = System.Web.Http.RouteAttribute;
-using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
 using RoutePrefixAttribute = System.Web.Http.RoutePrefixAttribute;
 using ActionNameAttribute = System.Web.Http.ActionNameAttribute;
+using Microsoft.Xrm.Sdk;
+using System.Web.Http.ModelBinding;
+using System.Diagnostics.Contracts;
+using System.CodeDom;
+using System.Reflection;
+using System.Web.UI.WebControls;
+using System.Web.Http.Controllers;
 
 namespace WebApplication1.Controllers
 {
     [RoutePrefix("api/values")]
     public class ValuesController : ApiController
     {
-        // GET api/values
-        [HttpGet]
-        [Route("GetAll")]
-        public IEnumerable<string> Get()
+
+        private bool ValidateHeaders(HttpRequestMessage request)
         {
-            return new string[] { "value1", "value2" };
+            if (!request.Headers.Contains("AppName") || !request.Headers.Contains("Token"))// || !request.Headers.Contains("Authorization"))
+            {
+                return false;
+            }
+
+            var appName = request.Headers.GetValues("AppName").FirstOrDefault();
+            var token = request.Headers.GetValues("Token").FirstOrDefault();
+           // var authorization = request.Headers.GetValues("Authorization").FirstOrDefault();
+
+          
+            if (appName != "MyApp" || token != "123456" )//|| authorization != "Bearer xyz")
+            {
+                return false;
+            }
+
+            return true;
         }
 
-        // GET api/values/5
-        [HttpGet]
-        [Route("{id:int}")]
-        public IHttpActionResult Get(int id)
+        [System.Web.Http.HttpGet]
+        [Route("{id:guid}")]
+        public IHttpActionResult Get(Guid id)
         {
-            var conn = new Connection();
-            var service = conn.GetCrmService();
-            var ent = service.Retrieve("contact", new Guid("A76FE889-852B-E811-80CC-005056B6C839"), new ColumnSet("contactid", "chr_identificationno"));
-            var customers = new List<Customer>();
-
-            if (ent != null)
+            if (!ValidateHeaders(Request))
             {
-                var customer = new Customer
-                {
-                    Id = new Guid(ent["contactid"].ToString()),
-                    IdentificationNo = ent["chr_identificationno"].ToString(),
-                    Type = true,
-                    Firstname = "aaa",
-                    Lastname = "aaaa"
-                };
-                customers.Add(customer);
-
-                customer = new Customer
-                {
-                    Id = new Guid(ent["contactid"].ToString()),
-                    IdentificationNo = ent["chr_identificationno"].ToString(),
-                    Type = true,
-                    Firstname = "bb",
-                    Lastname = "bb"
-                };
-                customers.Add(customer);
-
-                customer = new Customer
-                {
-                    Id = new Guid(ent["contactid"].ToString()),
-                    IdentificationNo = ent["chr_identificationno"].ToString(),
-                    Type = true,
-                    Firstname = "cc",
-                    Lastname = "cc"
-                };
-                customers.Add(customer);
+                return (IHttpActionResult)Unauthorized(); // خطای 401 برمی‌گرداند
             }
-            else
+            
+            Contact contact = null;
+            object result = null;
+            try
             {
-                return NotFound();
+                var conn = new Connection();
+                var service = conn.GetCrmService();
+                var ent = service.Retrieve("contact", id, new ColumnSet("contactid", "firstname", "lastname", "emailaddress1", "mobilephone"));
+                
+                if (ent != null)
+                {
+                    contact = new Contact()
+                    {
+                        Id = new Guid(ent["contactid"].ToString()),
+                        emailaddress1 = ent["emailaddress1"].ToString(),
+                        mobilephone = ent["mobilephone"].ToString(),
+                        Firstname = ent["firstname"].ToString(),
+                        Lastname = ent["lastname"].ToString()
+                    };
+                    result = new
+                    {
+                        data = contact,
+                        message = "Contact Found",
+                        statusCode = 200,
+                        succeed = true
+                    };
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
-
-            var result = new
+            catch (Exception ex)
             {
-                data = customers,
-                statusCode = 200,
-                succeed = true
-            };
+                result = new
+                {
+                    data = contact,
+                    message=ex.Message,
+                    statusCode = 500,
+                    succeed = false
+                };
+            }
+            
 
             return Ok(result); // برگرداندن شیء نهایی با data و statusCode
         }
-
         
         [System.Web.Http.HttpGet]
-        [Route("GetSag")]
-        public IHttpActionResult GetSag(string id)
+        [Route("GetContactByMobile")]
+        public IHttpActionResult GetContactByMobile(string mobile)
         {
             var conn = new Connection();
             var service = conn.GetCrmService();
             var query = new QueryExpression()
             {
-                ColumnSet=new ColumnSet("contactid", "chr_identificationno"),
-                EntityName="contact",
+                ColumnSet= new ColumnSet("contactid", "firstname", "lastname", "emailaddress1", "mobilephone"),
+                EntityName ="contact",
                 Criteria =
                 {
                     Filters=
@@ -107,93 +123,285 @@ namespace WebApplication1.Controllers
                         {
                             Conditions =
                             {
-                                new ConditionExpression("chr_identificationno",ConditionOperator.Equal,id)
+                                new ConditionExpression("mobilephone",ConditionOperator.Equal,mobile)
                             }
                         }
                     }
                 }
             };
-            var ent = service.RetrieveMultiple(query).Entities.FirstOrDefault();
 
-            var customers = new List<Customer>();
-
-            if (ent != null)
+            Contact contact = null;
+            object result = null;
+            try
             {
-                var customer = new Customer
+                var ent = service.RetrieveMultiple(query).Entities.FirstOrDefault();
+                if (ent != null)
                 {
-                    Id = new Guid(ent["contactid"].ToString()),
-                    IdentificationNo = ent["chr_identificationno"].ToString(),
-                    Type = true,
-                    Firstname = "aaa",
-                    Lastname = "aaaa"
+                    contact = new Contact
+                    {
+                        Id = new Guid(ent["contactid"].ToString()),
+                        emailaddress1 = ent["emailaddress1"].ToString(),
+                        mobilephone = ent["mobilephone"].ToString(),
+                        Firstname = ent["firstname"].ToString(),
+                        Lastname = ent["lastname"].ToString()
+                    };
+                    result = new
+                    {
+                        data = contact,
+                        message = "Contact Found",
+                        statusCode = 200,
+                        succeed = true
+                    };
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                result = new
+                {
+                    data = contact,
+                    message = ex.Message,
+                    statusCode = 500,
+                    succeed = false
                 };
-                customers.Add(customer);
-
             }
-            else
-            {
-                return NotFound();
-            }
-
-            var result = new
-            {
-                data = customers,
-                statusCode = 200,
-                succeed = true
-            };
-
             return Ok(result); // برگرداندن شیء نهایی با data و statusCode
         }
 
+        [System.Web.Http.HttpGet]
+        [Route("GetContactByName")]
+        public IHttpActionResult GetContactByName([FromBody] InputParametersForGetContact inp)
+        {
+            if (!ValidateHeaders(Request))
+            {
+                return (IHttpActionResult)Unauthorized(); // خطای 401 برمی‌گرداند
+            }
+            //
+            if (inp == null)
+            {
+                return BadRequest("input parameters is empty or null.");
+            }
+            var conn = new Connection();
+            var service = conn.GetCrmService();
+            var query = new QueryExpression();
+            query.ColumnSet = new ColumnSet("contactid", "firstname", "lastname", "emailaddress1", "mobilephone");
+            query.EntityName = "contact";
+            ConditionExpression condition1 = new ConditionExpression("firstname", ConditionOperator.Equal, inp.firstName);
+            ConditionExpression condition2 = new ConditionExpression("lastname", ConditionOperator.Equal, inp.lastName);
+            FilterExpression filter = new FilterExpression(LogicalOperator.And);
+            //By this way you can search with firstname or lastname or both of them
+            if (!inp.firstName.IsNullOrWhiteSpace())
+                filter.AddCondition(condition1);
+            if (!inp.lastName.IsNullOrWhiteSpace())
+                filter.AddCondition(condition2);
+            
+            query.Criteria.AddFilter(filter);
+
+            List<Contact> contacts = new List<Contact>();
+            object result = null;
+            try
+            {
+                var ent = service.RetrieveMultiple(query).Entities;
+                foreach (var item in ent)
+                {
+                    var contact = new Contact
+                    {
+                        Id = new Guid(item["contactid"].ToString()),
+                        emailaddress1 = item.GetAttributeValue<string>("emailaddress1"),
+                        mobilephone = item.GetAttributeValue<string>("mobilephone"),
+                        Firstname = item.GetAttributeValue<string>("firstname"),
+                        Lastname = item.GetAttributeValue<string>("lastname"),
+                    };
+                    contacts.Add(contact);
+                }
+                
+                result = new
+                {
+                    data = contacts,
+                    message = "Contact Found",
+                    statusCode = 200,
+                    succeed = true
+                };
+            }
+            catch (Exception ex)
+            {
+                result = new
+                {
+                    data = contacts,
+                    message = ex.Message,
+                    statusCode = 500,
+                    succeed = false
+                };
+            }
+            return Ok(result); // برگرداندن شیء نهایی با data و statusCode
+        }
         // POST api/values
         [System.Web.Http.HttpPost]
-        [Route("PostSag")]
-        public IHttpActionResult PostSag([FromBody] List<Customer> customers)
+        [Route("RegisterContact")]
+        public IHttpActionResult RegisterContact([FromBody] Contact contact)
         {
-            if (customers == null || customers.Count == 0)
+            if (contact == null)
             {
                 return BadRequest("Customer list is empty or null.");
             }
 
-            // پردازش داده‌های دریافتی
-            foreach (var customer in customers)
+            try
             {
-                // مثلاً ذخیره‌سازی هر مشتری در پایگاه داده
-                Console.WriteLine($"Received Customer: {customer.Firstname} {customer.Lastname}");
+                var conn = new Connection();
+                var service = conn.GetCrmService();
+                var ent = new Entity("contact");
+                ent["firstname"] = contact.Firstname;
+                ent["lastname"] = contact.Lastname;
+                ent["emailaddress1"] = contact.emailaddress1;
+                ent["mobilephone"] = contact.mobilephone;
+                contact.Id = service.Create(ent);
+                return Ok(new
+                {
+                    data = contact,
+                    message = "Contact created successfully",
+                    statusCode = 200,
+                    succeed = true
+                });
             }
-
-            return Ok(new
+            catch (Exception ex)
             {
-                message = "Customers processed successfully",
-                statusCode = true
-            });
+                return Ok(new
+                {
+                    data = contact,
+                    message = ex.Message,
+                    statusCode = 500,
+                    succeed = false
+                });
+            }
         }
 
         [System.Web.Http.HttpPost]
-        [Route("PostCat")]
-        public IHttpActionResult PostCat([FromBody] sag1 s)
+        [Route("RegisterMultiContacts")]
+        public IHttpActionResult RegisterMultiContacts([FromBody] List<Contact> contacts)
         {
-            if (s == null )
+            if (contacts == null || contacts.Count==0)
             {
-                return BadRequest("sag list is empty or null.");
+                return BadRequest("Contact list is empty or null.");
             }
-
-            // پردازش داده‌های دریافتی
             
-
-            return Ok(new
+            var conn = new Connection();
+            var service = conn.GetCrmService();
+            var ent = new Entity("contact");
+            List<Contact> createdContacts = new List<Contact>();
+            List<Guid> createdGUID = new List<Guid>();
+            int i = 1;
+            try
             {
-                message = "sag processed successfully",
-                statusCode = true
-            });
+                foreach (var contact in contacts)
+                {
+                    ent["firstname"] = contact.Firstname;
+                    ent["lastname"] = contact.Lastname;
+                    ent["emailaddress1"] = contact.emailaddress1;
+                    ent["mobilephone"] = contact.mobilephone;
+                    contact.Id = service.Create(ent);
+                    createdContacts.Add(contact);
+                    createdGUID.Add(contact.Id);
+                    i++;
+                }
+                
+                return Ok(new
+                {
+                    data = createdGUID,
+                    message = $"{i-1} Contacts created successfully",
+                    statusCode = 200,
+                    succeed = true
+                });
+            }
+            catch (Exception ex)
+            {
+                if (createdGUID.Count > 0)
+                    return Ok(new
+                    {
+                        data = createdGUID,
+                        message = $"{i-1} Contacts are created , but {ex.Message}",
+                        statusCode = 200,
+                        succeed =false
+                    });
+                else
+                    return Ok(new
+                    {
+                        data = createdGUID,
+                        message = $"Creation of Contacts failed",
+                        statusCode = 500,
+                        succeed = false
+                    });
+            }
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
+
+
+        [System.Web.Http.HttpPut]
+        [Route("UpdateContactByMobile")]
+        public IHttpActionResult UpdateContactByMobile(string mobile, [FromBody] Contact contact)
         {
+            var conn = new Connection();
+            var service = conn.GetCrmService();
+            var query = new QueryExpression()
+            {
+                ColumnSet = new ColumnSet("contactid", "firstname", "lastname", "emailaddress1", "mobilephone"),
+                EntityName = "contact",
+                Criteria =
+                {
+                    Filters=
+                    {
+                        new FilterExpression
+                        {
+                            Conditions =
+                            {
+                                new ConditionExpression("mobilephone",ConditionOperator.Equal,mobile)
+                            }
+                        }
+                    }
+                }
+            };
+
+
+            try
+            {
+                var ent = service.RetrieveMultiple(query).Entities.FirstOrDefault();
+                if (ent != null)
+                {
+                    if (!contact.Firstname.IsNullOrWhiteSpace())
+                        ent["firstname"] = contact.Firstname;
+                    if (!contact.Lastname.IsNullOrWhiteSpace())
+                        ent["lastname"] = contact.Lastname;
+                    if (!contact.mobilephone.IsNullOrWhiteSpace())
+                        ent["mobilephone"] = contact.mobilephone;
+                    if (!contact.mobilephone.IsNullOrWhiteSpace())
+                        ent["emailaddress1"] = contact.emailaddress1;
+                    service.Update(ent);
+
+                    return Ok(new
+                    {
+                        data = contact,
+                        message = "Update Successfuly",
+                        statusCode = 200,
+                        succeed = true
+                    });
+                }
+                else
+                    return NotFound();
+            }
+            catch(Exception ex)
+            {
+                return Ok(new
+                {
+                        data = new object(),
+                        message = ex.Message,
+                        statusCode = 500,
+                        succeed = false
+                });
+            }
         }
 
-        // DELETE api/values/5
         public void Delete(int id)
         {
         }
--- WebApplication1/Models/Customer.cs
+++ WebApplication1/Models/Customer.cs
@@ -5,18 +5,18 @@ using System.Web;
 
 namespace api.Models
 {
-        public class Customer
-        {
-            public bool Type { get; set; }
-            public string Firstname { get; set; }
-            public string Lastname { get; set; }
-            public string IdentificationNo { get; set; }
-            public Guid Id { get; set; }
-        }
-    public class sag1
+    public class Contact
     {
-        public string Id { get; set; }
-        public string name { get; set; }
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string emailaddress1 { get; set; }  
+        public string mobilephone { get; set; }
+    }
+    public class InputParametersForGetContact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
     }
     
 }
